59a71a53fbbf7ea62fcca1708ad32833e495745e
Danny <57437166+DanielW898723589@users.noreply.github.com>
2020-05-19 11:44:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Rechner.Core;
+using System;
+
+namespace _18052020_rechenautomat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Rechenautomat dw = new Rechenautomat();
+
+            string eingabe = "-";
+            while (eingabe != "")
+            {
+                var nextRechnung = dw.NächsteBerechnung();
+
+                Console.Write("Ladevorgang..." + nextRechnung + "-");
+            }
+        }
+    }
+}
