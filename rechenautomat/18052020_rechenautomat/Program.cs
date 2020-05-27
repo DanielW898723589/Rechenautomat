235ee5d357912c8d868dc668db4f453a03c6158b
Danny <57437166+DanielW898723589@users.noreply.github.com>
2020-05-27 12:42:56 +0200

    rechenautomat27052020

---
+++ rechenautomat/18052020_rechenautomat/Program.cs
@@ -0,0 +1,31 @@
+﻿using Rechner.Core;
+using System;
+
+namespace _18052020_rechenautomat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Rechenautomat dw = new Rechenautomat();
+
+            string eingabe = "-";
+            while (eingabe != "")
+            {
+                var nextRechnung = dw.NächsteBerechnung();
+
+                Console.Write("Ladevorgang..." + nextRechnung + "-");
+
+                int eingabe2 = Convert.ToInt32(Console.ReadLine());
+
+
+                if (dw.Prüfe(eingabe2) == false)
+                {
+                    throw new Exception("Diese Lösung ist falsch!");
+                }
+            }
+
+        }
+    }
+}
