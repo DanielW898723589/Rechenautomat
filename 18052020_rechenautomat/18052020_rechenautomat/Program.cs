0a861492693a90e11dd59ef5f0feeb3d3a1eb6c8
Danny <57437166+DanielW898723589@users.noreply.github.com>
2020-05-27 11:59:54 +0200

    Rechenautomat vvs

---
+++ 18052020_rechenautomat/18052020_rechenautomat/Program.cs
@@ -0,0 +1,31 @@
+﻿using Rechner.Core;
+using System;
+
+namespace _18052020_rechenautomat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Rechenautomat dw = new Rechenautomat();
+
+            string eingabe = "-";
+            while (eingabe != "")
+            {
+                var nextRechnung = dw.NächsteBerechnung();
+
+                Console.Write("Ladevorgang..." + nextRechnung + "-");
+
+                int eingabe = Convert.ToInt32(Console.ReadLine());
+
+
+                if (dw.Prüfe(eingabe) == false)
+                {
+                    throw new Exception("Diese Lösung ist falsch!");
+                }
+            }
+
+        }
+    }
+}
